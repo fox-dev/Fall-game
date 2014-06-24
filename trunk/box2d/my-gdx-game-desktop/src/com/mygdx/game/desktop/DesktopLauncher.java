4fb346832c11655516a5dc129a323a9bf2c95981
aabriam <aabriam@4e269b95-df6b-0001-712e-0a9950f8c787>
2014-06-24 03:51:21 +0000

    Test box2d

---
+++ trunk/box2d/my-gdx-game-desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,20 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.main.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		
+		config.title = Game.TITLE;
+		config.width = Game.V_WIDTH*Game.SCALE;
+		config.height = Game.V_HEIGHT*Game.SCALE;
+		
+		new LwjglApplication(new Game(), config);
+		
+		
+	}
+}
